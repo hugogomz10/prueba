3a9171b5c20a04887fe7ad0984824ae6032684d4
Hugo Gomez <72419821+hugogomz10@users.noreply.github.com>
2022-09-28 14:04:17 -0500

    Updated files

--- scripts/script.js
+++ scripts/script.js
@@ -144,40 +144,82 @@ pressButton9.addEventListener('click', function(){
 });
 
 //BOTONES DE CONCEPTOS CLAVE
-//Boton Intro
-var btnAbrirPopup = document.getElementById('intro'),
+//Boton Primer Concepto
+var btnAbrirPopup = document.getElementById('first'),
     overlay = document.getElementById('overlay'),
     popup = document.getElementById('popup'),
     btnCerrarPopup = document.getElementById('btncerrar');
-    //ocultarCarta = document.getElementById('btntap');
 
 btnAbrirPopup.addEventListener('click', function(){
     overlay.classList.add('active');
     popup.classList.add('active');
-    //ocultarCarta.classList.add('active');
 });
 
 btnCerrarPopup.addEventListener('click', function(){
     overlay.classList.remove('active');
     popup.classList.remove('active');
-    //ocultarCarta.classList.remove('active');
 });
 
-//Boton Objetivos
-var btnAbrirPopup2 = document.getElementById('objt'),
+//Boton Segundo Concepto
+var btnAbrirPopup2 = document.getElementById('second'),
     overlay2 = document.getElementById('overlay2'),
     popup2 = document.getElementById('popup2'),
     btnCerrarPopup2 = document.getElementById('btncerrar2');
-    //ocultarCarta2 = document.getElementById('btntap2');
 
 btnAbrirPopup2.addEventListener('click', function(){
     overlay2.classList.add('active');
     popup2.classList.add('active');
-    //ocultarCarta2.classList.add('active');
 });
 
 btnCerrarPopup2.addEventListener('click', function(){
     overlay2.classList.remove('active');
     popup2.classList.remove('active');
-    //ocultarCarta2.classList.remove('active');
+});
+
+//Boton Tercer Concepto
+var btnAbrirPopup3 = document.getElementById('third'),
+    overlay3 = document.getElementById('overlay3'),
+    popup3 = document.getElementById('popup3'),
+    btnCerrarPopup3 = document.getElementById('btncerrar3');
+
+btnAbrirPopup3.addEventListener('click', function(){
+    overlay3.classList.add('active');
+    popup3.classList.add('active');
+});
+
+btnCerrarPopup3.addEventListener('click', function(){
+    overlay3.classList.remove('active');
+    popup3.classList.remove('active');
+});
+
+//Boton Cuarto Concepto
+var btnAbrirPopup4 = document.getElementById('fourth'),
+    overlay4 = document.getElementById('overlay4'),
+    popup4 = document.getElementById('popup4'),
+    btnCerrarPopup4 = document.getElementById('btncerrar4');
+
+btnAbrirPopup4.addEventListener('click', function(){
+    overlay4.classList.add('active');
+    popup4.classList.add('active');
+});
+
+btnCerrarPopup4.addEventListener('click', function(){
+    overlay4.classList.remove('active');
+    popup4.classList.remove('active');
+});
+
+//Boton Quinto Concepto
+var btnAbrirPopup5 = document.getElementById('fifth'),
+    overlay5 = document.getElementById('overlay5'),
+    popup5 = document.getElementById('popup5'),
+    btnCerrarPopup5 = document.getElementById('btncerrar5');
+
+btnAbrirPopup5.addEventListener('click', function(){
+    overlay5.classList.add('active');
+    popup5.classList.add('active');
+});
+
+btnCerrarPopup5.addEventListener('click', function(){
+    overlay5.classList.remove('active');
+    popup5.classList.remove('active');
 });
